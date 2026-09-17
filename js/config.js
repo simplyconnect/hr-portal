b6304b06d9d9d48bbcaa4320854abc21e4bfe2e7
simplyconnect <shahab.uddin@wesimcon.com>
2026-09-17 12:27:14 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -46,7 +46,7 @@
 const APP_CONFIG = {
   USE_MOCK_DATA: false,
 
-  GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/AKfycbxEaCcNCk4mWvRcQFdkj3k2HihXYkbMKyvYYGRYqdtw0EDzi5XJfzqFsqI-9JRqn9gV/exec",
+  GOOGLE_SHEETS_API_URL: "https://script.google.com/macros/s/AKfycbwaowph3AwSKiDV_qL8Hkv2QYjMiygSNWYTIAKUs13K_Q53aV6olBW5JAu4HwQiXP_rxQ/exec",
   BIOMETRIC_MIDDLEWARE_URL: "https://REPLACE_WITH_YOUR_LOCAL_MIDDLEWARE/api",
 
   COMPANY_NAME: "Simply Connect",
